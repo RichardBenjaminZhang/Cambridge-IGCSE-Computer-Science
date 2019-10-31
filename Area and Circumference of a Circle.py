eb9f9f89d7613450d4002f3d32bd8453172e95d9
JoshuaMcmaster <56988000+JoshuaMcmaster@users.noreply.github.com>
2019-10-31 09:16:52 +0800

    Add files via upload

---
+++ Area and Circumference of a Circle.py
@@ -0,0 +1,6 @@
+import math
+r=float(input("You should enter radius now: "))
+p=2*math.pi*r    #Circle's circumference
+s=math.pi*r**2    #Circle's area
+print("Circle's circumference: ",p)
+print("Circle's area: ",s)
---
+++ multiply_demo.py
@@ -0,0 +1,12 @@
+# multiply_demo.py
+
+# Request and store user input
+number1=int(input("Please insert first number:"))
+number2=int(input("Please insert second number:"))
+
+result=number1 * number2
+# Display the value held in the variable result
+print("The answer is ",result)
+
+# End nicely by waiting for the user to press the return key.
+input("/n/nPress RETURN to finish.")
