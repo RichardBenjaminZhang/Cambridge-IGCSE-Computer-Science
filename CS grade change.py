0b95b0e5f9b6c57a5a4837e25298288537725cf2
JoshuaMcmaster <56988000+JoshuaMcmaster@users.noreply.github.com>
2019-11-13 15:02:53 +0800

    Add files via upload

---
+++ CS grade change.py
@@ -0,0 +1,17 @@
+Grade=float (input("Can I know your point?"))
+if Grade == 5:
+    print("Your Grade is A")
+if Grade ==4:
+    print("Your Grade is B")
+if Grade ==3:
+    print("Your Grade is C")
+if Grade ==2:
+    print("Your Grade is D")
+if Grade ==1:
+    print("Your Grade is F")
+if Grade ==0:
+    print("Your Grade is F")
+if Grade >5:
+    print("You're think peach,The full score only 5!")
+# End nicely by waiting for the user to press the return key.
+input("/n/nPress RETURN to finish.")
