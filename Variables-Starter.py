c944bb2493042f888e9394f7982a8951c0f95913
JoshuaMcmaster <56988000+JoshuaMcmaster@users.noreply.github.com>
2019-11-22 14:52:47 +0800

    Add files via upload

---
+++ Variables-Starter.py
@@ -0,0 +1,12 @@
+first = 2
+second = 3
+third = first * second
+print(third)
+input("\n\nPress enter keep moving.")
+second = third - first
+first = first + second + third
+print(first)
+input("\n\nPress enter keep moving.")
+third = second * first
+print(third)
+input("\n\nPress RETURN to finish.")
