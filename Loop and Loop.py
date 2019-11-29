263611dd00f8b44e274984348b68aef2b1c4df8b
JoshuaMcmaster <56988000+JoshuaMcmaster@users.noreply.github.com>
2019-11-29 15:09:22 +0800

    Add files via upload

---
+++ Loop and Loop.py
@@ -0,0 +1,5 @@
+total=0
+num=int(input("Enter one number?"))
+while num != -1:
+    total = total+num
+    print('Your total is: ' ,total)
---
+++ Loop number.py
@@ -0,0 +1,6 @@
+total=0
+num=0
+while num != -1:
+    num=int(input("Enter one number?"))
+    total = total+num
+print('Your total is: ' ,total)
