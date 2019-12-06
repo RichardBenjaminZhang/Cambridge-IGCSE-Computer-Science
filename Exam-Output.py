a46ec8de6c2ef5c05a72ace39fb0a871c6aa57ba
JoshuaMcmaster <56988000+JoshuaMcmaster@users.noreply.github.com>
2019-12-06 10:33:58 +0800

    Add files via upload

---
+++ Exam-Output.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+
+temp=x
+x=y
+y=temp
+print("the first number is: ",x)
+print("the second number is: ",y)
+input("/n/nPress RETURN to finish.")
