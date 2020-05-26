76c77943ff80269529d72554b49c5248ef9398fd
JoshuaMcmaster <56988000+JoshuaMcmaster@users.noreply.github.com>
2020-05-26 10:24:56 +0800

    Add files via upload

---
+++ Christmas Tree.py
@@ -0,0 +1,6 @@
+for i in range(1,7,1):
+    for j in range(7,i,-1):
+        print(" ",end="")
+    for j in range(0,i,1):
+        print("* ",end="")
+    print()
---
+++ Nested Loop3.py
@@ -0,0 +1,6 @@
+for i in range(1,6):
+    for j in range(5-i,0,-1):
+        print('',end="")
+    for k in range(0,i,1):
+        print(i,end="")
+    print()
