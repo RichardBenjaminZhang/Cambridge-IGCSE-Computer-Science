8f4d416531430198649592f1c9eb588578b143a8
JoshuaMcmaster <56988000+JoshuaMcmaster@users.noreply.github.com>
2020-12-03 09:15:31 +0800

    Update 2020_0478_q2_SectionA.py

--- 2020_0478_q2_SectionA.py
+++ 2020_0478_q2_SectionA.py
@@ -27,10 +27,18 @@ Total_Score=[0]*Size
 for i in range(Size):
     Total_Score[i]=Marks_1[i]+Marks_2[i]+Marks_3[i]
     print(Total_Score[i])
+    print(namesArr[i])
 Class_Total=0
 for i in range(Size):
     Class_Total=Class_Total+Total_Score[i]
 average=Class_Total/Size
 print(average)
-
 #Task3
+def select():
+    Highest_Score=0
+    for i in range(Size):
+        if Highest_Score <Total_Score[i]:
+            Highest_Score=Total_Score[i]
+    #print("The Student name is",namesArr[i])
+    print("This student mark is",Highest_Score)
+select()
