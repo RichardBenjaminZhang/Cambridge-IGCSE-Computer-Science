4460f4cf76001b67ed5c2f5ea7ca21add8323477
JoshuaMcmaster <56988000+JoshuaMcmaster@users.noreply.github.com>
2021-01-20 20:13:34 +0800

    Bug_Report_Issue_1
    Just for test no downloa

---
+++ pre_m_21_Eroor.py
@@ -0,0 +1,39 @@
+#Task1
+TicketCost=25.00
+Trains_Up_Time=[9,11,13,15]
+Trains_Up_Tickets=[480,480,480,480]
+Trains_Up_Money=[0.0,0.0,0.0,0.0]
+Trains_Down_Time=[10,12,14,16]
+Trains_Down_Tickets=[480,480,480,640]
+Trains_Down_Money=[0.0,0.0,0.0,0.0]
+print("Trian Times\t\tTickets Avaiable \tTotal Money for this time")
+for count in range(0,4):
+    print(Trains_Up_Time[count],"\t\t\t",Trains_Up_Tickets[count],"\t\t\t",Trains_Up_Money[count])
+    print(Trains_Down_Time[count],"\t\t\t",Trains_Down_Tickets[count],"\t\t\t",Trains_Down_Money[count])
+#Task2
+Ticket_Sell="Y"
+while Ticket_Sell=="Y":
+    TimeUp=int(input("What time period ticket you wanna buy? It's 9:00/11:00/13:00/15:00"))
+while TimeUp!=9 and TimeUp!=11 and TimeUp!=13 and TimeUp!=15:
+    TimeUp=int(input("Sorry. The Time is not acceptble."))
+for count in range(0,4):
+    if TimeUp==Trains_Up_Time[count]:
+        IndexUp = count
+TimeDown=int(input("What time period ticket you wanna buy? It's 10:00/12:00/14:00/16:00"))
+while TimeDown<TimeUp or (TimeDown!=10 and TimeDown!=12 and TimeDown!=14 and TimeDown!=16):
+    TimeDown = int(input("Sorry. The Time is not acceptble."))
+for count in range(0,4):
+    if TimeDown==Trains_Down_Time[count]:
+        IndexDown = count
+NumTickets=int(input("How many do you wanna buy? The tenth tickets is free."))
+while NumTickets > Trains_Up_Tickets[IndexUp] or NumTickets > Trains_Down_Tickets[IndexDown]:
+    NumTickets = int(input("Sorry. The Ticket is not acceptble."))
+Trians_Up_Tickets[IndexUp]=Trians_Up_Tickets[IndexUp]-NumTickets
+Trians_Down_Tickets[IndexDown]=Trians_Down_Tickets[IndexDown]-NumTickets
+for count in range(0,4):
+    if Trians_Up_Tickets[count]==0:
+        Trians_Up_Tickets[count]=str（Trians_Up_Tickets[count]）
+        Trians_Up_Tickets[count]="Closed"
+    if Trians_Down_Tickets[count]==0:
+        Trians_Down_Tickets[count]=str（Trians_Down_Tickets[count]）
+        Trians_Down_Tickets[count]="Closed"
