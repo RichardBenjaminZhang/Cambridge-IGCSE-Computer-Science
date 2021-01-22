6fd37194f53f5252b7d7dbaf03bba8fe5c553ff3
JoshuaMcmaster <56988000+JoshuaMcmaster@users.noreply.github.com>
2021-01-22 11:23:00 +0800

    Add files via upload

---
+++ pre_m_2021(Easy).py
@@ -0,0 +1,21 @@
+Cost=25
+Coach_Size=80
+Time_Range=8
+Total_Passengers=0
+Total_Cost=0.0
+Coach_1=[0]*TIME_RANGE
+Coach_2=[0]*TIME_RANGE
+Coach_3=[0]*TIME_RANGE
+Coach_4=[0]*TIME_RANGE
+Coach_5=[0]*TIME_RANGE
+Coach_6=[0]*(TIME_RANGE+2)
+available=480
+
+def task_1():
+    Total_Up=0.0
+    Total_Down=0.0
+    print()
+    print("*******Tickets Info*******")
+    print("Up Time:9:00,11:00,13:00,15:00")
+    print("Down Time:10:00,12:00,14:00,16:00")
+    print("Avalilable Tickets",avalilable)
